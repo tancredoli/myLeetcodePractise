333cf613bef98594a613cb3c889c32b510a555d9
Rui Li <tancredo29@gmail.com>
2021-02-24 15:36:17 -0500

    update leetcode prob 240.

--- Python/240. Search a 2D Matrix  II/solution.py
+++ Python/240. Search a 2D Matrix  II/solution.py
@@ -3,9 +3,11 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
         i, j = len(matrix) - 1, 0
 
         while i >= 0 and j < len(matrix[0]):
+
             if target == matrix[i][j]:
                 return True
             elif target > matrix[i][j]:
