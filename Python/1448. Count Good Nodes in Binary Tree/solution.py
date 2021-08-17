b5707b64b7399cba8f86fce13c4bb6cdcab8f62c
Rui Li <tancredo29@gmail.com>
2021-08-17 13:08:01 -0400

    update leetcode prob 1448.

---
+++ Python/1448. Count Good Nodes in Binary Tree/solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.res = 0
+
+        def dfs(node, maximum):
+            if node.val >= maximum:
+                self.res += 1
+                maximum = node.val
+            if node.left:
+                dfs(node.left, maximum)
+            if node.right:
+                dfs(node.right, maximum)
+
+        dfs(root, -float('inf'))
+
+        return self.res
